62adac46b0d1cc1318e3ba61288a8344917ea63d
Maayavi786 <aroos.alm@gmail.com>
2025-04-17 21:43:17 +0300

    fix(types): revert Request interface augmentation, only extend SessionData for express-session TS compatibility

--- netlify/functions/types/express-session/index.d.ts
+++ netlify/functions/types/express-session/index.d.ts
@@ -1,11 +1,7 @@
 // TypeScript module augmentation for express-session
 declare module 'express-session' {
-  import { Session, SessionData } from 'express-session';
   interface SessionData {
     user?: any;
     // Add any additional properties you store on session
-  }
-  interface Request {
-    session: Session & Partial<SessionData>;
   }
 }
