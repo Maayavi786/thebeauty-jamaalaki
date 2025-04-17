b1e8046bc9cf7b603f270fce973b9f39b9820958
Maayavi786 <aroos.alm@gmail.com>
2025-04-17 21:28:12 +0300

    fix(types): augment express-session to add session property to Request interface for TS support

--- netlify/functions/types/express-session/index.d.ts
+++ netlify/functions/types/express-session/index.d.ts
@@ -1,6 +1,11 @@
 // TypeScript module augmentation for express-session
 declare module 'express-session' {
+  import { Session, SessionData } from 'express-session';
   interface SessionData {
     user?: any;
+    // Add any additional properties you store on session
+  }
+  interface Request {
+    session: Session & Partial<SessionData>;
   }
 }
